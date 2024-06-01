6d90c311bd3972e794b80acb3397b2bfb194a321
admin <hoangchinhcva@gmail.com>
2024-06-01 11:21:05 +0700

    CRUD LoanType

--- express-service/controllers/loanTypeController.js
+++ express-service/controllers/loanTypeController.js
@@ -11,6 +11,55 @@ let createLoanType = async (req, res) => {
         })
     }
 }
+let getAllLoanType = async (req, res) => {
+    try {
+        let infor = await loanTypeService.getAllLoanType();
+        return res.status(200).json({ infor })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EC: -1,
+            EM: 'Error from server'
+        })
+    }
+}
+let getLoanTypeById = async (req, res) => {
+    try {
+        let infor = await loanTypeService.getAllLoanType(req.query.id);
+        return res.status(200).json({ infor })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EC: -1,
+            EM: 'Error from server'
+        })
+    }
+}
+let editLoanType = async (req, res) => {
+    try {
+        let infor = await loanTypeService.editLoanType(req.body);
+        return res.status(200).json({ infor })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EC: -1,
+            EM: 'Error from server'
+        })
+    }
+}
+let deleteLoanType = async (req, res) => {
+    try {
+        console.log(req.body.id)
+        let infor = await loanTypeService.deleteLoanType(req.body.id);
+        return res.status(200).json({ infor })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EC: -1,
+            EM: 'Error from server'
+        })
+    }
+}
 module.exports = {
-    createLoanType,
+    createLoanType, getAllLoanType, getLoanTypeById, editLoanType, deleteLoanType,
 }
--- express-service/routes/index.js
+++ express-service/routes/index.js
@@ -4,8 +4,11 @@ let router = express.Router();
 let initWebRouters = (app) => {
 
     //LoanType
-    router.get('/create-loan-type', loanTypeController.createLoanType)
-
+    router.get('/api/create-loan-type', loanTypeController.createLoanType)
+    router.get('/api/get-all-loan-type', loanTypeController.getAllLoanType)
+    router.get('/api/get-loan-type-by-id', loanTypeController.getLoanTypeById)
+    router.put('/api/edit-loan-type', loanTypeController.editLoanType)
+    router.delete('/api/delete-loan-type', loanTypeController.deleteLoanType)
     return app.use("/", router)
 }
 
