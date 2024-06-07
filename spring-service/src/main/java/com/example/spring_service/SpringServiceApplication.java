7505b820a10cf5a8af07ec034d0ab278d0f01f5b
Nhân  Nguyễn <nhan.nguyen3@bnksolution.com>
2024-06-07 14:53:38 +0700

    feat: make a call api from spring service to express service

--- spring-service/src/main/java/com/example/spring_service/SpringServiceApplication.java
+++ spring-service/src/main/java/com/example/spring_service/SpringServiceApplication.java
@@ -2,11 +2,17 @@ package com.example.spring_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServiceApplication.class, args);
 	}
-}
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
